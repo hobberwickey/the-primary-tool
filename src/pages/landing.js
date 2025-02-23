5de353caaddfdd8dedd5b79b45124101dcd74246
Joel <>
2025-02-23 13:27:35 -0500

    Cleaning up styles

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -16,69 +16,79 @@ export const LandingPage = () => {
 
   return (
     <section id="landing-page" className="page">
-      <div className="lander"></div>
-      <div className="lander-content">
-        <h2>It's Our Government, Make Your Voice Heard</h2>
-        <form>
-          <div className="autocomplete input-wrapper">
-            <h3>To get started, please enter your address</h3>
-            <input
-              value={search || address || ""}
-              onInput={(e) => {
-                setSearch(e.target.value);
-              }}
-            />
-            {!!search && results.length && (
-              <ul className="uk-list results">
-                {results.map((result) => {
-                  return (
-                    <li
-                      onClick={() => {
-                        handleAddress(result);
-                      }}
+      <div className="flex-block">
+        <div className="lander"></div>
+        <div className="lander-content">
+          <h2>It's Our Government, Make Your Voice Heard</h2>
+          <form>
+            <div className="autocomplete input-wrapper">
+              <h3>To get started, please enter your address</h3>
+              <input
+                value={search || address || ""}
+                onInput={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              {!!search && results.length && (
+                <ul className="uk-list results">
+                  {results.map((result) => {
+                    return (
+                      <li
+                        onClick={() => {
+                          handleAddress(result);
+                        }}
+                      >
+                        {result}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+          </form>
+          {!!address && (
+            <div>
+              <h3>I want to</h3>
+
+              <div className="actions">
+                <div>
+                  <div>
+                    <p>Find candidates in my district</p>
+
+                    <a
+                      className="btn blocky"
+                      href="/voters"
+                      alt="Find Candidates"
                     >
-                      {result}
-                    </li>
-                  );
-                })}
-              </ul>
-            )}
-          </div>
-        </form>
-        {!!address && (
-          <div>
-            <h3>I want to</h3>
-
-            <div className="actions">
-              <div>
-                <div>
-                  <p>Find candidates in my district</p>
-
-                  <a
-                    className="btn blocky"
-                    href="/voters"
-                    alt="Find Candidates"
-                  >
-                    Go
-                  </a>
+                      Go
+                    </a>
+                  </div>
                 </div>
-              </div>
-              <div>
                 <div>
-                  <p>Become a candidate</p>
-                  <a
-                    className="btn blocky"
-                    href="/candidate"
-                    alt="Become a Candidate"
-                  >
-                    Go
-                  </a>
+                  <div>
+                    <p>Become a candidate</p>
+                    <a
+                      className="btn blocky"
+                      href="/candidate"
+                      alt="Become a Candidate"
+                    >
+                      Go
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
-        ;
+          )}
+          ;
+        </div>
+      </div>
+      <div>
+        <p className="parenthetical">
+          There's no reason we can't give voters all the tools they need to make
+          informed decisions about the candidates in their district and give
+          candidates all the tools they need to run a vital, and successful
+          campaign all for free.
+        </p>
       </div>
     </section>
   );
