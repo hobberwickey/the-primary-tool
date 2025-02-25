d1e369d14fcf4759e405be304e065d3b38bb1a12
Maya Nigrin <maya.nigrin@gmail.com>
2025-02-25 03:48:21 -0500

    touch up styles

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -17,13 +17,18 @@ export const LandingPage = () => {
   return (
     <section id="landing-page" className="page">
       <div className="flex-block">
-        <div className="lander"></div>
+        <img
+          className="lander"
+          src="/images/replace-landing-image.webp"
+          alt="Landing page image"
+        />
         <div className="lander-content">
           <h2>It's Our Government, Make Your Voice Heard</h2>
           <form>
             <div className="autocomplete input-wrapper">
-              <h3>To get started, please enter your address</h3>
+              <h3 for="address">To get started, please enter your address</h3>
               <input
+                name="address"
                 value={search || address || ""}
                 onInput={(e) => {
                   setSearch(e.target.value);
@@ -46,40 +51,22 @@ export const LandingPage = () => {
               )}
             </div>
           </form>
-          {!!address && (
-            <div>
+            <div style={{visibility: address ? 'visible' : 'hidden'}}>
               <h3>I want to</h3>
 
               <div className="actions">
-                <div>
-                  <div>
-                    <p>Find candidates in my district</p>
-
-                    <a
-                      className="btn blocky"
-                      href="/voters"
-                      alt="Find Candidates"
-                    >
-                      Go
-                    </a>
-                  </div>
-                </div>
-                <div>
-                  <div>
-                    <p>Become a candidate</p>
-                    <a
-                      className="btn blocky"
-                      href="/candidate"
-                      alt="Become a Candidate"
-                    >
-                      Go
-                    </a>
-                  </div>
-                </div>
+                <a className="btn blocky" href="/voters" alt="Find Candidates">
+                  Find candidates in my district
+                </a>
+                <a
+                  className="btn blocky"
+                  href="/candidate"
+                  alt="Become a Candidate"
+                >
+                  Become a candidate
+                </a>
               </div>
             </div>
-          )}
-          ;
         </div>
       </div>
       <div>
