d162ae80e9a61a4b144002e5b6087c4738178f3c
Maya Nigrin <maya.nigrin@gmail.com>
2025-02-25 02:26:59 -0500

    make width more reactive

--- src/App.js
+++ src/App.js
@@ -14,23 +14,23 @@ import { AppProviders } from "./providers";
 function App() {
   return (
     <AppProviders>
-        <header>
-          <nav data-uk-navbar>
-            <div className="uk-navbar-left">
-              <ul className="uk-navbar-nav">
-                <li className="uk-active">
-                  <a href="/" className="logo">
-                    <h2>Democracy Hub</h2>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="uk-navbar-right">
-              <div className="btn blocky rev">Sign In</div>
-            </div>
-          </nav>
-        </header>
-
+      <header>
+        <nav data-uk-navbar>
+          <div className="uk-navbar-left">
+            <ul className="uk-navbar-nav">
+              <li className="uk-active">
+                <a href="/" className="logo">
+                  <h2>Democracy Hub</h2>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="uk-navbar-right">
+            <div className="btn blocky rev">Sign In</div>
+          </div>
+        </nav>
+      </header>
+      <div className="content">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LandingPage />} />
@@ -46,6 +46,7 @@ function App() {
             */}
           </Routes>
         </BrowserRouter>
+      </div>
     </AppProviders>
   );
 }
